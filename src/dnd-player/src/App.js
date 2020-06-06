21527c804c6809327d52cc9db2869e35097b85ac
Ravenor222 <ndanvers222@gmail.com>
2020-06-06 13:28:36 -0700

    started to add front end, but its not necessary so scaling back

--- src/dnd-player/src/App.js
+++ src/dnd-player/src/App.js
@@ -4,19 +4,22 @@ import './App.css';
 import axios from 'axios'
 
 function App() {
-  const [inventory,setInventory] = useState({
-    
-  })
+  const [inventoryState,setInventoryState] = useState([])
+
+
 const handleRoot = () => {
   axios.get("http://localhost:8081/")
   .then((res) => console.log(res))
 }
 const handleAllItems = () => {
-  axios.get("http://localhost:8081/all")
-  .then((res) => console.log(res))
+  axios.get("http://localhost:8081/all/")
+  .then((res) => {
+    setInventoryState(res.data);
+    console.log(inventoryState);
+  })
 };
 
-const handleAdd = (name, itemtype) => {
+const handleAddItems = (name, itemtype) => {
   axios.post(`http://localhost:8081/add/${name}/${itemtype}`)
   .then((res) => console.log(res))
 };
@@ -36,12 +39,19 @@ const handleUpdate = (itemtype, name) => {
 
   return (
     <div className="App">
-      <button onClick={handleRoot}>Click me for all</button>
+      <button onClick={handleRoot}>Click me for rooot</button>
      <button onClick={handleAllItems}>Click me for all</button>
-     <button>Click me for add</button>
+     <button onClick={handleAddItems("axe", "weapon")}>Click me to add</button>
      <button>Click me for remove</button>
      <button>Click me for update</button>
 
+      <div className="map-division">
+        {inventoryState.map((x) => <div style={{
+        border:"2px solid black",
+        height:"100px",
+        width:"100px",
+        }}><p>{x.Name}</p><p>{x.ItemType}</p></div>)}
+      </div>
 
     </div>
   );
